e80f15c2264c22930cfd94964759f1fe1b493aa6
Lara <lara.brockdorff.17@um.edu.mt>
2019-10-08 11:43:53 +0200

    adding calac empty 0 tests

--- src/main/java/edu/uom/StringCalculator.java
+++ src/main/java/edu/uom/StringCalculator.java
@@ -2,11 +2,13 @@ package edu.uom;
 
 public class StringCalculator {
 
-//    public int add(String numbers){
-//
-//        if (numbers == null || numbers.length()==0){
-//            System.out.println("Empty String");
-//            return 0;
-//        }
-//    }
+    public int add(String numbers){
+
+        if (numbers == null || numbers.trim().length()==0){
+            System.out.println("Empty String");
+            return 0;
+        }
+        return -1;
+
+    }
 }
---
+++ src/test/java/edu/uom/StringCalculatorTest.java
@@ -0,0 +1,46 @@
+package edu.uom;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class StringCalculatorTest {
+    StringCalculator calc ;
+
+    @Before
+    public void setup(){
+        calc = new StringCalculator();
+    }
+
+    @After
+    public void teardown (){
+        calc = null;
+    }
+
+    @Test
+    public void add_withEmptyString(){
+
+        //exercise
+        int result = calc.add("");
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void add_withNull(){
+
+        //exercise
+        int result = calc.add(null);
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void add_withWhiteSpcae(){
+
+        //exercise
+        int result = calc.add("   \t \n \n");
+        assertEquals(0,result);
+    }
+
+}
