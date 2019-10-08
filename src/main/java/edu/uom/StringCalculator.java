d44771efff4a471aa031d9030da30527ad0fec63
Lara <lara.brockdorff.17@um.edu.mt>
2019-10-08 11:25:42 +0200

    starting string calc ex 2

---
+++ src/main/java/edu/uom/StringCalculator.java
@@ -0,0 +1,12 @@
+package edu.uom;
+
+public class StringCalculator {
+
+//    public int add(String numbers){
+//
+//        if (numbers == null || numbers.length()==0){
+//            System.out.println("Empty String");
+//            return 0;
+//        }
+//    }
+}
