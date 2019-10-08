9694f2d329d44e81b0380ffd0cf6c7eff81a8ece
Lara <lara.brockdorff.17@um.edu.mt>
2019-10-08 11:01:02 +0200

    adding tests for coverage with and before and after

--- src/main/java/edu/uom/HelloWorld.java
+++ src/main/java/edu/uom/HelloWorld.java
@@ -2,8 +2,15 @@ package edu.uom;
 
 public class HelloWorld {
 
-    public String getMessgage(){
+    public String getMessage(){
         return "Hello World!!";
+    }
+
+    public  String getMessage(String name){
+        if(name == null|| name.length()==0){
+            return getMessage();
+        }
+        return "Hello " + name +"!!";
     }
 
 }
--- src/test/java/edu/uom/HelloWorldTest.java
+++ src/test/java/edu/uom/HelloWorldTest.java
@@ -1,25 +1,70 @@
 package edu.uom;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
 
+    HelloWorld helloWorld;
+    @Before
+    public void setup(){
+        //Setup
+        helloWorld= new HelloWorld();
+    }
+
+    @After
+    public void teardown(){
+        //Teardown
+        helloWorld = null; // will actually be done automatically by garbage collector (just here to show teardown example)
+
+    }
+
     @Test
     public void testStandardMessage(){
-        //Setup
-        HelloWorld helloWorld = new HelloWorld();
-
 
         //Exercise
-        String msg = helloWorld.getMessgage();
+        String msg = helloWorld.getMessage();
 
         //Verify
         assertEquals("Hello World!!", msg);
 
-        //Teardown
-        helloWorld = null; // will actually be done automatically by garbage collector (just here to show teardown example)
     }
 
+    @Test
+    public void testStandardMessage_withName(){
+
+        //Exercise
+        String msg = helloWorld.getMessage("Lara");
+
+        //Verify
+        assertEquals("Hello Lara!!", msg);
+
+    }
+
+
+    @Test
+    public void testStandardMessage_withNameNull(){
+        
+        //Exercise
+        String msg = helloWorld.getMessage(null);
+
+        //Verify
+        assertEquals("Hello World!!", msg);
+
+    }
+
+
+    @Test
+    public void testStandardMessage_withNameEmpty(){
+
+        //Exercise
+        String msg = helloWorld.getMessage("");
+
+        //Verify
+        assertEquals("Hello World!!", msg);
+
+ }
 }
